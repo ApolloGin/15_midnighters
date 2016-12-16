239eb90a93cbdd8a755fb38300ef7aaf82f2827c
Toxagin <toxagin@gmail.com>
2016-12-16 22:54:02 +0300

    midnighters emplements and adds documentation

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # 15_midnighters
+
+## Synopsis
+
+Script helps to understand whom on site devman.com sent challenges after 00:00.
+
+## Quick start
+
+ - Install all requirements from file `pip3 install -r requirements.txt`
+ - Run seek_dev_nighters.py `python3.5 seek_dev_nighters.py`
+ - **Input**: nothing
+ - **Output**: list of users and time of sending challenges
--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,34 @@
-def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+import requests
+import pytz
+import datetime
 
-def get_midnighters():
-    pass
+def load_attempts():
+    api_template = 'https://devman.org/api/challenges/solution_attempts/'\
+    '?page={page}'
+    response = requests.get(api_template.format(page=1))
+    pages = response.json()['number_of_pages']
+    for page in range(1,pages + 1):
+        response = requests.get(api_template.format(page=page))
+        for record in response.json()['records']:
+            yield record
+
+def get_midnighters(records):
+    for record in records:
+        date_time = get_datetime(record['timestamp'], record['timezone'])
+        if date_time and date_time.hour in range(1,5):
+            yield record['username'], date_time
+
+def get_datetime(timestamp, tz):
+    if timestamp is None or tz is None:
+        return None
+    return datetime.datetime.fromtimestamp(timestamp, pytz.timezone(tz))
 
 if __name__ == '__main__':
-  pass
+    for username, date_time in get_midnighters(load_attempts()):
+        print(
+            'User "{0}" sends challenge {1} at {2}'.format(
+                username,
+                date_time.strftime('%d-%m-%Y'),
+                date_time.strftime('%H:%M %Z')
+            )
+        )
